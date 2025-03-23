c9b6f8e3eadc7f256875bc461889a72579e0313f
yuvlian <138542238+yuvlian@users.noreply.github.com>
2025-03-23 18:31:35 +0700

    macro sdk router

--- sdk-sv/src/app/router.rs
+++ sdk-sv/src/app/router.rs
@@ -1,126 +1,60 @@
 use crate::ArcState;
+use crate::handlers::{auth, dispatch, misc};
 use axum::{
     Router,
     routing::{get, post},
 };
+use paste::paste;
 
-use crate::handlers::{auth, dispatch, misc};
+macro_rules! gen_router {
+    ($prefix:ident; $($route:literal $method:ident $handler:ident);* $(;)?) => {
+        paste! {
+            pub fn [<$prefix _router>]() -> Router<ArcState> {
+                let mut r = Router::new();
 
-pub fn auth_router() -> Router<ArcState> {
-    let mut r = Router::new();
+                $(
+                    r = r.route($route, $method($prefix::$handler::$method));
+                )*
 
-    r = r.route("/account/register", get(auth::account_register::get));
-
-    r = r.route("/account/register", post(auth::account_register::post));
-
-    r = r.route(
-        "/{product}/combo/granter/login/beforeVerify",
-        post(auth::combo_before_verify::post),
-    );
-
-    r = r.route(
-        "/account/risky/api/check",
-        post(auth::risky_api_check::post),
-    );
-
-    r = r.route(
-        "/{product}/mdk/shield/api/login",
-        post(auth::shield_api_login::post),
-    );
-
-    r = r.route(
-        "/{product}/mdk/shield/api/verify",
-        post(auth::shield_api_verify::post),
-    );
-
-    r = r.route(
-        "/{product}/combo/granter/login/v2/login",
-        post(auth::combo_granter_login::post),
-    );
-
-    r
+                r
+            }
+        }
+    };
 }
 
-pub fn dispatch_router() -> Router<ArcState> {
-    let mut r = Router::new();
+gen_router![
+    auth;
+    "/account/register" get account_register;
+    "/account/register" post account_register;
+    "/account/risky/api/check" post risky_api_check;
+    "/{product}/combo/granter/login/beforeVerify" post combo_before_verify;
+    "/{product}/mdk/shield/api/login" post shield_api_login;
+    "/{product}/mdk/shield/api/verify" post shield_api_verify;
+    "/{product}/combo/granter/login/v2/login" post combo_granter_login
+];
 
-    r = r.route("/query_dispatch", get(dispatch::query_dispatch::get));
+gen_router![
+    dispatch;
+    "/query_dispatch" get query_dispatch;
+    "/query_gateway" get query_gateway
+];
 
-    r = r.route("/query_gateway", get(dispatch::query_gateway::get));
-
-    r
-}
-
-pub fn misc_router() -> Router<ArcState> {
-    let mut r = Router::new();
-
-    r = r.route("/admin/mi18n/{*remainder}", get(misc::admin_mi18n::get));
-
-    r = r.route(
-        "/{product}/combo/granter/api/compareProtocolVersion",
-        post(misc::compare_protocol_version::post),
-    );
-
-    r = r.route(
-        "/data_abtest_api/config/experiment/list",
-        post(misc::config_experiment_list::post),
-    );
-
-    r = r.route(
-        "/combo/box/api/config/sdk/combo",
-        get(misc::config_sdk_combo::get),
-    );
-
-    r = r.route(
-        "/device-fp/api/getExtList",
-        get(misc::device_get_ext_list::get),
-    );
-
-    r = r.route("/device-fp/api/getFp", post(misc::device_get_fp::post));
-
-    r = r.route(
-        "/{product}/mdk/agreement/api/getAgreementInfos",
-        get(misc::get_agreement_infos::get),
-    );
-
-    r = r.route(
-        "/common/{product}/announcement/api/getAlertAnn",
-        get(misc::get_alert_ann::get),
-    );
-
-    r = r.route(
-        "/common/{product}/announcement/api/getAlertPic",
-        get(misc::get_alert_pic::get),
-    );
-
-    r = r.route(
-        "/common/{product}/announcement/api/getAnnList",
-        get(misc::get_ann_list::get),
-    );
-
-    r = r.route(
-        "/{product}/combo/granter/api/getConfig",
-        get(misc::granter_api_get_config::get),
-    );
-
-    r = r.route("/common/h5log/log/batch", post(misc::h5log::post));
-
-    r = r.route(
-        "/{product}/combo/red_dot/list",
-        post(misc::red_dot_list::post),
-    );
-
-    r = r.route("/sdk/dataUpload", post(misc::sdk_data_upload::post));
-
-    r = r.route(
-        "/{product}/mdk/shield/api/loadConfig",
-        get(misc::shield_api_load_config::get),
-    );
-
-    r = r.route(
-        "/combo/box/api/config/sw/precache",
-        get(misc::sw_precache::get),
-    );
-
-    r
-}
+gen_router![
+    misc;
+    "/admin/mi18n/{*remainder}" get admin_mi18n;
+    "/common/{product}/announcement/api/getAlertAnn" get get_alert_ann;
+    "/common/{product}/announcement/api/getAlertPic" get get_alert_pic;
+    "/common/{product}/announcement/api/getAnnList" get get_ann_list;
+    "/common/h5log/log/batch" post h5log;
+    "/combo/box/api/config/sdk/combo" get config_sdk_combo;
+    "/combo/box/api/config/sw/precache" get sw_precache;
+    "/data_abtest_api/config/experiment/list" post config_experiment_list;
+    "/device-fp/api/getExtList" get device_get_ext_list;
+    "/device-fp/api/getFp" post device_get_fp;
+    "/{product}/combo/granter/api/compareProtocolVersion" post compare_protocol_version;
+    "/{product}/combo/granter/api/getConfig" get granter_api_get_config;
+    "/{product}/mdk/agreement/api/getAgreementInfos" get get_agreement_infos;
+    "/{product}/mdk/shield/api/loadConfig" get shield_api_load_config;
+    "/{product}/combo/red_dot/list" post red_dot_list;
+    "/sdk/dataUpload" post sdk_data_upload;
+];
