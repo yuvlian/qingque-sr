b692b040cba2f2f8db1c523b0bdcd53916f9b7b1
yuvlian <yuvlian@naver.com>
2025-08-17 12:02:59 +0700

    shield logins

--- sdkserver/auth/auth.go
+++ sdkserver/auth/auth.go
@@ -6,11 +6,37 @@ func AddHandlers(h *http.ServeMux) {
 	h.HandleFunc("/account/risky/api/check", riskyCheckHandler)
 	h.HandleFunc("/hkrpg_global/combo/granter/login/v2/login", tokenLoginHandler)
 	h.HandleFunc("/hkrpg_global/account/ma-passport/api/appLoginByPassword", passwordLoginHandler)
+	h.HandleFunc("/hkrpg_global/mdk/shield/api/login", fuckingShieldThingsForBeta)
+	h.HandleFunc("/hkrpg_global/mdk/shield/api/verify", fuckingShieldThingsForBeta)
 }
 
 func riskyCheckHandler(w http.ResponseWriter, r *http.Request) {
 	response := `{
 		"data": {},
+		"message": "OK",
+		"retcode": 0
+	}`
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(response))
+}
+
+// not even needed for prod this shit pisses me off
+func fuckingShieldThingsForBeta(w http.ResponseWriter, r *http.Request) {
+	response := `{
+		"data": {
+			"account": {
+				"area_code": "**",
+				"country": "ID",
+				"email": "yuvlian@naver.com",
+				"is_email_verify": "1",
+				"token": "x",
+				"uid": "1"
+			},
+			"device_grant_required": false,
+			"reactivate_required": false,
+			"realperson_required": false,
+			"safe_mobile_required": false
+		},
 		"message": "OK",
 		"retcode": 0
 	}`
